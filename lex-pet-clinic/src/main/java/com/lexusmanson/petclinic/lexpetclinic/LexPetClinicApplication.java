1a3241429d12c4ee3e39c6e1d55b52ca1e4b048f
Lex-Man <1795498+Lex-Man@users.noreply.github.com>
2019-10-18 11:05:06 +0100

    initial commit

---
+++ lex-pet-clinic/src/main/java/com/lexusmanson/petclinic/lexpetclinic/LexPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.lexusmanson.petclinic.lexpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LexPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LexPetClinicApplication.class, args);
+	}
+
+}
---
+++ lex-pet-clinic/src/test/java/com/lexusmanson/petclinic/lexpetclinic/LexPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexusmanson.petclinic.lexpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LexPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
